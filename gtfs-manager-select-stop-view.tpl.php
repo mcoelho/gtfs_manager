10576b8404d7ec43ea7c3e1415f65874cd4b9aad
sagefranch <sage.franch@outlook.com>
2014-07-08 11:01:57 -0300

    Created template for stop selection view

---
+++ gtfs-manager-select-stop-view.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**file template for selecting stops
+*/
+
+//links should be: "Back"
+print implode(' | ', $variables['links']);
+
+?>
+
+<h3>Select a Stop from Existing</h3>
+<!--<p>Enter the latitude and longitude of the stop you want to select:</p>
+text boxes
+<p>Available stops: </p>
+drop down list of generated stops-->
+
+<!-- call the functions from stop.pages.inc to load forms for selection-->
+<!--gtfs_manager_stop_select()-->
+<!--gtfs_manager_stop_form_additional_info()-->
